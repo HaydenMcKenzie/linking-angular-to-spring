a2aa9861603eaa30af19ef377df9702cc037ecec
HaydenMcKenzie <Haydendanielmckenzie@gmail.com>
2023-09-30 20:25:02 +1000

    setup

---
+++ src/main/java/com/surfproject/springboot/mainTest.java
@@ -0,0 +1,12 @@
+package com.surfproject.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class mainTest {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Hello World";
+    }
+}
